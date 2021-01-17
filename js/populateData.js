41bbd16647a308e1ad2ef39c13547c22ea5491c5
coronavirusliveitalia <sasso0101@gmail.com>
2021-01-17 13:20:59 +0100

    Fix znes

--- js/populateData.js
+++ js/populateData.js
@@ -48,19 +48,19 @@ function updateData() {
             document.getElementById('lastUpdated').innerHTML=newLastUpdated;
             
             /* Current restrictions map */
-            redDays = ['5/1/2021', '6/1/2021'];
-            orangeDays = ['9/1/2021', '10/1/2021'];
-            yellowDays = ['7/1/2021', '8/1/2021'];
-            todayObj = new Date();
-            today = todayObj.getDate()+'/'+(todayObj.getMonth()+1)+'/'+todayObj.getFullYear();
-            pastDate = new Date(2020, 1, 10);
-            if (redDays.includes(today)) document.getElementById('restrinctionsMap').src = 'maps/zonesRedBig.svg';
-            else if (yellowDays.includes(today)) document.getElementById('restrinctionsMap').src = 'maps/zonesYellowBig.svg';
-            else if (todayObj > pastDate) {
+            // redDays = ['5/1/2021', '6/1/2021'];
+            // orangeDays = ['9/1/2021', '10/1/2021'];
+            // yellowDays = ['7/1/2021', '8/1/2021'];
+            // todayObj = new Date();
+            // today = todayObj.getDate()+'/'+(todayObj.getMonth()+1)+'/'+todayObj.getFullYear();
+            // pastDate = new Date(2020, 1, 10);
+            // if (redDays.includes(today)) document.getElementById('restrinctionsMap').src = 'maps/zonesRedBig.svg';
+            // else if (yellowDays.includes(today)) document.getElementById('restrinctionsMap').src = 'maps/zonesYellowBig.svg';
+            // else if (todayObj > pastDate) {
                 document.getElementById('restrictionsSelection').style.display = 'none';
-                document.getElementById('restrinctionsMap').src = 'maps/zones10Big.svg';
-            }
-            else document.getElementById('restrinctionsMap').src = 'maps/zonesOrangeBig.svg';
+                document.getElementById('restrinctionsMap').src = 'maps/zones17Big.svg';
+            // }
+            // else document.getElementById('restrinctionsMap').src = 'maps/zonesOrangeBig.svg';
 
             /* Restriction calendar */
             populateCalendar();
