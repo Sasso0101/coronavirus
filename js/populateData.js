111d246d353fb9d36f3c0391132b28a2e87d17d2
coronavirusliveitalia <sasso0101@gmail.com>
2021-02-09 08:20:52 +0100

    Update map

--- js/populateData.js
+++ js/populateData.js
@@ -58,7 +58,7 @@ function updateData() {
             // else if (yellowDays.includes(today)) document.getElementById('restrinctionsMap').src = 'maps/zonesYellowBig.svg';
             // else if (todayObj > pastDate) {
                 document.getElementById('restrictionsSelection').style.display = 'none';
-                document.getElementById('restrinctionsMap').src = 'maps/zones.svg?_=3';
+                document.getElementById('restrinctionsMap').src = 'maps/zones.svg?_=4';
             // }
             // else document.getElementById('restrinctionsMap').src = 'maps/zonesOrangeBig.svg';
 
